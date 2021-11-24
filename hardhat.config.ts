d7d5356d10f2115393fdeae01a1d8de8e4032c20
0xRudolf <0xrudolf@protonmail.com>
2021-11-24 01:18:41 +0100

    :wrench: set config for bsc testnet, mainnet and etherscan

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,15 +1,42 @@
+import fs from "fs";
 import "@typechain/hardhat";
 import "@nomiclabs/hardhat-waffle";
 import "@nomiclabs/hardhat-ethers";
+import "@nomiclabs/hardhat-etherscan";
 import { HardhatUserConfig } from "hardhat/config";
+
+let accountTestnet;
+const accountTestnetPath = "./secrets/accounts.testnet.json";
+if (fs.existsSync(accountTestnetPath)) accountTestnet = require(accountTestnetPath);
+
+let accountMainnet;
+const accountMainnetPath = "./secrets/accounts.mainnet.json";
+if (fs.existsSync(accountMainnetPath)) accountMainnet = require(accountMainnetPath);
+
+let etherscan;
+const etherscanScanPath = "./secrets/etherscan.json";
+if (fs.existsSync(etherscanScanPath)) etherscan = require(etherscanScanPath);
 
 const config: HardhatUserConfig = {
   networks: {
-    hardhat: {
-      initialDate: "2021-01-01",
+    bsc_testnet: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+      chainId: 97,
+      gasPrice: 20000000000,
+      ...(accountTestnet ? { accounts: accountTestnet } : {})
     },
+    bsc_mainnet: {
+      url: "https://bsc-dataseed.binance.org/",
+      chainId: 56,
+      gasPrice: 20000000000,
+      ...(accountMainnet ? { accounts: accountMainnet } : {})
+    },
+    hardhat: {
+      initialDate: "2021-01-01"
+    }
   },
-  solidity: "0.8.4",
+  ...(etherscan ? { etherscan } : {}),
+  solidity: "0.8.4"
 };
 
 export default config;
